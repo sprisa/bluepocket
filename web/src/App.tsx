8beb1a0c645f6608c0cf1db23e931fde9d145e7d
Gabe M <hey@gabe.mx>
2025-06-18 14:04:48 -0600

    scafollding for atproto db

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { AtpAgent } from "@atproto/api";
+import { sha256 } from "js-sha256";
+
+const agent = new AtpAgent({
+  service: "https://bsky.social",
+  persistSession(ev, session) {
+    console.log("persist", { ev, session });
+    if (session == null) return;
+    localStorage.setItem("session", JSON.stringify(session));
+  },
+});
+
+const existingSession = localStorage.getItem("session");
+if (existingSession != null) {
+  try {
+    const session = JSON.parse(existingSession);
+    console.log("session", session);
+    const res = await agent.resumeSession(session);
+    if (res.success === false) {
+      localStorage.removeItem("session");
+    }
+  } catch (err) {
+    localStorage.removeItem("session");
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  console.log("did", agent.did);
 
   return (
     <>
@@ -16,20 +42,104 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>BluePocket</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <form
+          style={{ display: "flex", flexDirection: "column", gap: 16 }}
+          action={async (ev) => {
+            const username = ev.get("username");
+            const password = ev.get("password");
+            console.log({ username, password });
+            if (typeof username !== "string" || typeof password !== "string")
+              return;
+
+            const res = await agent
+              .login({
+                identifier: username,
+                password: password,
+              })
+              .catch((err) => {
+                console.log("error login", err);
+              });
+            console.log({ res });
+          }}
+        >
+          <label htmlFor="username">Username</label>
+          <input id="username" name="username" />
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" />
+          <button type="submit">Login</button>
+        </form>
+
+        <button
+          onClick={async () => {
+            const url =
+              "https://www.atproto-browser.dev/at/did:plc:saeh7cmd5rq2ikufc2zw5pap";
+            const rkey = sha256(url);
+
+            const saveRes = await agent.com.atproto.repo.putRecord({
+              repo: agent.assertDid, // The user
+              collection: "org.bluepocket.v1.save", // The collection
+              rkey: rkey, // The record key
+              record: {
+                url: url,
+                state: "saved",
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString(),
+              },
+            });
+            console.log("put", saveRes);
+
+            const favRes = await agent.com.atproto.repo.putRecord({
+              repo: agent.assertDid,
+              collection: "org.bluepocket.v1.favorite",
+              rkey: rkey, // Collection name
+              record: {
+                subject: {
+                  cid: saveRes.data.cid,
+                  uri: saveRes.data.uri,
+                }
+              },
+            });
+            console.log("favRes", favRes);
+
+            const collectionName = "test"
+            const collectionRes = await agent.com.atproto.repo.putRecord({
+              repo: agent.assertDid,
+              collection: "org.bluepocket.v1.collection",
+              rkey: collectionName, // Collection name
+              record: {
+                description: "My fun collection of links",
+              },
+            });
+
+            console.log("collection", collectionRes);
+
+            // agent.com.atproto.repo.listRecords({
+
+            // })
+
+            const addCollectionRes = await agent.com.atproto.repo.putRecord({
+              repo: agent.assertDid,
+              collection: `org.bluepocket.v1.collection.${collectionName}`,
+              rkey: rkey, // Collection name
+              record: {
+                note: "Interesting read.",
+                subject: {
+                  cid: saveRes.data.cid,
+                  uri: saveRes.data.uri,
+                },
+              },
+            });
+
+            console.log("collection save", addCollectionRes);
+          }}
+        >
+          Put Record
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
